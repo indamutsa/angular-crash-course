8d446afb7ffee0624ab1617930f72b86ea4cd437
indamutsa <arsichizy@gmail.com>
2024-04-06 13:04:41 +0200

    Using an observable though with asynchronous data using the local data

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -17,6 +17,6 @@ export class TasksComponent {
   constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks();
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 }
