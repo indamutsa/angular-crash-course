3395d8c109676d6fe2204304a30bd26c3e0eaeed
indamutsa <arsichizy@gmail.com>
2024-04-06 19:23:56 +0200

    Adding http client to call the server

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TASKS } from '../mock-tasks';
 import { Task } from '../Task';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  // constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
     const tasks = of(TASKS);
